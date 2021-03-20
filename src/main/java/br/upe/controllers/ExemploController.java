574eaf545861528b9c2818d1dee328aae5c95825
Daniel Barros Sales <daniel.b.sales@outlook.com>
2021-03-20 16:00:36 -0300

    Running project

--- src/main/java/br/upe/controllers/ExemploController.java
+++ src/main/java/br/upe/controllers/ExemploController.java
@@ -17,8 +17,8 @@ public class ExemploController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/views/index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
