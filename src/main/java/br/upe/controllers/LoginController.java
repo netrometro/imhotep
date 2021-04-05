b906ac4d5bef8a37ae2f90c24d9794ae0e3eecf1
abraaodeveloper <abraaobarbosati@gmail.com>
2021-04-05 17:44:31 -0300

    remove folder bin

--- src/main/java/br/upe/controllers/LoginController.java
+++ src/main/java/br/upe/controllers/LoginController.java
@@ -56,6 +56,8 @@ public class LoginController extends HttpServlet {
 
             if (erros.size() == 0) {
 
+
+
                 try {
 
                     URI dbUri = new URI(System.getenv("DATABASE_URL"));
