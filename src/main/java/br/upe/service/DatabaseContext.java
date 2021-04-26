68810cafacd99801321252a61f727e6d4789dd93
Daniel Barros Sales <sales.b.daniel@gmail.com>
2021-04-26 00:00:30 -0300

    Closes #32 Creates table/entity and Dbset

--- src/main/java/br/upe/service/DatabaseContext.java
+++ src/main/java/br/upe/service/DatabaseContext.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import br.upe.model.entities.ConsultationPeriods;
 import br.upe.model.entities.User;
 import br.upe.model.entities.UserRole;
 import br.upe.service.core.DbSet;
@@ -13,11 +14,20 @@ public class DatabaseContext {
     private Connection conn;
     private IDbSet<UserRole> userRoles;
     private IDbSet<User> users;
+    private IDbSet<ConsultationPeriods> consultationPeriods;
 
     public DatabaseContext(Connection databaseConnection){        
         conn = databaseConnection;
         userRoles = new DbSet<UserRole>(databaseConnection, UserRole.class);
         users = new DbSet<User>(databaseConnection, User.class);
+    }
+
+    public IDbSet<ConsultationPeriods> getConsultationPeriods() {
+        return consultationPeriods;
+    }
+
+    public void setConsultationPeriods(IDbSet<ConsultationPeriods> consultationPeriods) {
+        this.consultationPeriods = consultationPeriods;
     }
 
     public IDbSet<UserRole> getUserRoles() {
@@ -42,6 +52,8 @@ public class DatabaseContext {
         String command = "BEGIN; CREATE TABLE user_roles (id serial PRIMARY KEY, name varchar(20) NOT NULL); CREATE TABLE users (id serial PRIMARY KEY, name varchar(100) NOT NULL" +
                          ", email varchar NOT NULL, cpf varchar(11) NOT NULL, birthday date NOT NULL, password varchar NOT NULL, crm varchar, user_role_id integer, CONSTRAINT " +
                          "fk_user_role FOREIGN KEY(user_role_id) REFERENCES user_roles(id));" +
+                         "CREATE TABLE consultation_periods (id serial PRIMARY KEY, sunday varchar, monday varchar, tuesday varchar, wednesday varchar," +
+                         " thursday varchar, friday varchar, saturday varchar, CONSTRAINT fk_user FOREIGN KEY(id) REFERENCES users(id));" + 
                          "INSERT INTO user_roles(name) values('"+UserRole.DOCTOR+"');" +
                          "INSERT INTO user_roles(name) values('"+UserRole.EMPLOYEER+"');" +
                          "INSERT INTO user_roles(name) values('"+UserRole.PATIENT+"');" +
@@ -52,7 +64,7 @@ public class DatabaseContext {
 
     public void ensureDeletedDatabase() throws SQLException {
         Statement st = conn.createStatement();
-        String command = "BEGIN; DROP TABLE IF EXISTS users; DROP TABLE IF EXISTS user_roles; END TRANSACTION;";
+        String command = "BEGIN; DROP TABLE IF EXISTS consultation_periods; DROP TABLE IF EXISTS users; END TRANSACTION; DROP TABLE IF EXISTS user_roles;";
         st.execute(command);
         st.execute("COMMIT;");
     }
